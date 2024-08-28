fea23a4113248886488c62809ea2dcc6f2c6d043
serkankorkut17 <srknkrktgs@gmail.com>
2024-08-28 04:25:31 +0300

    nav done

--- src/components/Navigation/NavigationBar.js
+++ src/components/Navigation/NavigationBar.js
@@ -37,7 +37,7 @@ const NavigationBar = () => {
                 >
                   <path d="M7.978 4a2.553 2.553 0 0 0-1.926.877C4.233 6.7 3.699 8.751 4.153 10.814c.44 1.995 1.778 3.893 3.456 5.572 1.68 1.679 3.577 3.018 5.57 3.459 2.062.456 4.115-.073 5.94-1.885a2.556 2.556 0 0 0 .001-3.861l-1.21-1.21a2.689 2.689 0 0 0-3.802 0l-.617.618a.806.806 0 0 1-1.14 0l-1.854-1.855a.807.807 0 0 1 0-1.14l.618-.62a2.692 2.692 0 0 0 0-3.803l-1.21-1.211A2.555 2.555 0 0 0 7.978 4Z" />
                 </svg>
-                <p>Phone: {contactInfo.phone}</p>
+                <a className="hover:text-orange-500 transition-colors duration-300" href={contactInfo.phone.link}>Phone: {contactInfo.phone.number}</a>
               </div>
               <div className="flex items-center space-x-1">
                 <svg
@@ -49,13 +49,13 @@ const NavigationBar = () => {
                   <path d="M2.038 5.61A2.01 2.01 0 0 0 2 6v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6c0-.12-.01-.238-.03-.352l-.866.65-7.89 6.032a2 2 0 0 1-2.429 0L2.884 6.288l-.846-.677Z" />
                   <path d="M20.677 4.117A1.996 1.996 0 0 0 20 4H4c-.225 0-.44.037-.642.105l.758.607L12 10.742 19.9 4.7l.777-.583Z" />
                 </svg>
-                <p>Email: {contactInfo.email}</p>
+                <a className="hover:text-orange-500 transition-colors duration-300" href={contactInfo.email.link}>Email: {contactInfo.email.address}</a>
               </div>
             </div>
             <div className="flex space-x-3">
               <Link href={socialLinks[0].url}>
                 <svg
-                  className="w-6 h-6 text-white"
+                  className="w-6 h-6 text-white hover:text-orange-500 transition-colors duration-300"
                   xmlns="http://www.w3.org/2000/svg"
                   fill="currentColor"
                   viewBox="0 0 24 24"
@@ -69,7 +69,7 @@ const NavigationBar = () => {
               </Link>
               <Link href={socialLinks[1].url}>
                 <svg
-                  className="w-6 h-6 text-white"
+                  className="w-6 h-6 text-white hover:text-orange-500 transition-colors duration-300"
                   xmlns="http://www.w3.org/2000/svg"
                   fill="currentColor"
                   viewBox="0 0 24 24"
@@ -83,7 +83,7 @@ const NavigationBar = () => {
               </Link>
               <Link href={socialLinks[2].url}>
                 <svg
-                  className="w-6 h-6 text-white"
+                  className="w-6 h-6 text-white hover:text-orange-500 transition-colors duration-300"
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
@@ -98,7 +98,7 @@ const NavigationBar = () => {
               </Link>
               <Link href={socialLinks[3].url}>
                 <svg
-                  className="w-6 h-6 text-white"
+                  className="w-6 h-6 text-white hover:text-orange-500 transition-colors duration-300"
                   xmlns="http://www.w3.org/2000/svg"
                   fill="currentColor"
                   viewBox="0 0 24 24"
@@ -121,7 +121,7 @@ const NavigationBar = () => {
               {navLinks.map((link) => (
                 <li key={link.name} className="md:mt-0">
                 <Link href={link.url}>
-                  <span className="hover:text-orange-500 cursor-pointer">
+                  <span className="hover:text-orange-500 transition-colors duration-300 cursor-pointer">
                   {link.name}
                   </span>
                 </Link>
