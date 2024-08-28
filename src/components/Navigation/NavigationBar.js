3e2f3b7b37fdf2454e0193a6b99b5740da20506c
serkankorkut17 <srknkrktgs@gmail.com>
2024-08-28 05:50:17 +0300

    hero ...

--- src/components/Navigation/NavigationBar.js
+++ src/components/Navigation/NavigationBar.js
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import MobileNavigation from "./MobileNavigation";
-import DRNDigitalLogo from "@/images/drndigital_white.png";
+import Logo from "@/images/drndigital_white.png";
 // import useTranslation from "@/hooks/useTranslation";
 import NavData from "@/data/nav.json";
 import { Montserrat } from "next/font/google";
@@ -23,50 +23,76 @@ const NavigationBar = () => {
     <nav className={`${montserrat.className} text-white py-2 px-4 `}>
       <div className="max-w-screen-xl mx-auto lg:mx-24 xl:mx-48 flex justify-between md:justify-evenly items-center">
         <div className="flex space-x-3">
-          <img
-            src={DRNDigitalLogo.src}
-            className="h-28"
-            alt="DRN Digital Logo"
-          />
+          <img src={Logo.src} className="h-28" alt="DRN Digital Logo" />
           {/* <span className="text-2xl font-semibold">Serkan Korkut</span> */}
         </div>
         <div className="md:basis-3/4 flex flex-col md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
           <MobileNavigation />
           <div className="hidden md:flex items-center justify-between border-b border-zinc-800 pb-2 mb-2">
             <div className="flex items-center space-x-4 text-sm">
-              <div className="flex items-center space-x-1">
-                <svg
-                  className="w-6 h-6 text-white"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M7.978 4a2.553 2.553 0 0 0-1.926.877C4.233 6.7 3.699 8.751 4.153 10.814c.44 1.995 1.778 3.893 3.456 5.572 1.68 1.679 3.577 3.018 5.57 3.459 2.062.456 4.115-.073 5.94-1.885a2.556 2.556 0 0 0 .001-3.861l-1.21-1.21a2.689 2.689 0 0 0-3.802 0l-.617.618a.806.806 0 0 1-1.14 0l-1.854-1.855a.807.807 0 0 1 0-1.14l.618-.62a2.692 2.692 0 0 0 0-3.803l-1.21-1.211A2.555 2.555 0 0 0 7.978 4Z" />
-                </svg>
-                <a
-                  className="hover:text-orange-500 transition-colors duration-300"
-                  href={contactInfo.phone.link}
-                >
-                  Phone: {contactInfo.phone.number}
-                </a>
-              </div>
-              <div className="flex items-center space-x-1">
-                <svg
-                  className="w-6 h-6 text-white"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M2.038 5.61A2.01 2.01 0 0 0 2 6v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6c0-.12-.01-.238-.03-.352l-.866.65-7.89 6.032a2 2 0 0 1-2.429 0L2.884 6.288l-.846-.677Z" />
-                  <path d="M20.677 4.117A1.996 1.996 0 0 0 20 4H4c-.225 0-.44.037-.642.105l.758.607L12 10.742 19.9 4.7l.777-.583Z" />
-                </svg>
-                <a
-                  className="hover:text-orange-500 transition-colors duration-300"
-                  href={contactInfo.email.link}
-                >
-                  Email: {contactInfo.email.address}
-                </a>
-              </div>
+              {contactInfo.phone.show && (
+                <div className="flex items-center space-x-1">
+                  <svg
+                    className="w-6 h-6 text-white"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M7.978 4a2.553 2.553 0 0 0-1.926.877C4.233 6.7 3.699 8.751 4.153 10.814c.44 1.995 1.778 3.893 3.456 5.572 1.68 1.679 3.577 3.018 5.57 3.459 2.062.456 4.115-.073 5.94-1.885a2.556 2.556 0 0 0 .001-3.861l-1.21-1.21a2.689 2.689 0 0 0-3.802 0l-.617.618a.806.806 0 0 1-1.14 0l-1.854-1.855a.807.807 0 0 1 0-1.14l.618-.62a2.692 2.692 0 0 0 0-3.803l-1.21-1.211A2.555 2.555 0 0 0 7.978 4Z" />
+                  </svg>
+                  <a
+                    className="hover:text-orange-500 transition-colors duration-300"
+                    href={contactInfo.phone.link}
+                  >
+                    Phone: {contactInfo.phone.number}
+                  </a>
+                </div>
+              )}
+              {contactInfo.email.show && (
+                <div className="flex items-center space-x-1">
+                  <svg
+                    className="w-6 h-6 text-white"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M2.038 5.61A2.01 2.01 0 0 0 2 6v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6c0-.12-.01-.238-.03-.352l-.866.65-7.89 6.032a2 2 0 0 1-2.429 0L2.884 6.288l-.846-.677Z" />
+                    <path d="M20.677 4.117A1.996 1.996 0 0 0 20 4H4c-.225 0-.44.037-.642.105l.758.607L12 10.742 19.9 4.7l.777-.583Z" />
+                  </svg>
+                  <a
+                    className="hover:text-orange-500 transition-colors duration-300"
+                    href={contactInfo.email.link}
+                  >
+                    Email: {contactInfo.email.address}
+                  </a>
+                </div>
+              )}
+              {contactInfo.address.show && (
+                <div className="flex items-center space-x-1">
+                  <svg
+                    class="w-6 h-6 text-white"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M11.906 1.994a8.002 8.002 0 0 1 8.09 8.421 7.996 7.996 0 0 1-1.297 3.957.996.996 0 0 1-.133.204l-.108.129c-.178.243-.37.477-.573.699l-5.112 6.224a1 1 0 0 1-1.545 0L5.982 15.26l-.002-.002a18.146 18.146 0 0 1-.309-.38l-.133-.163a.999.999 0 0 1-.13-.202 7.995 7.995 0 0 1 6.498-12.518ZM15 9.997a3 3 0 1 1-5.999 0 3 3 0 0 1 5.999 0Z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+
+                  <a
+                    className="hover:text-orange-500 transition-colors duration-300"
+                    href={contactInfo.address.link}
+                  >
+                    Location: {contactInfo.address.text}
+                  </a>
+                </div>
+              )}
             </div>
             <div className="flex space-x-3">
               <Link href={socialLinks[0].url}>
